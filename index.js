b499480d8529c2dccc0537836a076b37aa56d21c
shudipto <shudipta15-11424@diu.edu.bd>
2021-11-12 21:36:28 +0600

    first commit with 40% complete code

---
+++ index.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.upabr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+
+    try {
+        await client.connect();
+        console.log('hitting db');
+        const database = client.db('nakshi');
+        const productsCollection = database.collection('products');
+        const usersCollection = database.collection('users');
+        const ordersCollection = database.collection('orders');
+        console.log(productsCollection);
+
+        // user functions ===============================
+
+        // get api all products
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            console.log('hiiting all products');
+            res.send(products);
+        });
+
+        // get single products 
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        });
+
+        //post api add user
+        app.post('/adduser', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        // get single user 
+        app.get('/users/:email', async (req, res) => {
+            const mail = req.params.email;
+            console.log(req.params)
+            const query = { email: mail };
+            const user = await usersCollection.findOne(query);
+            res.send(user);
+        });
+
+
+
+        //update pending status
+        app.put('/updatePending', async (req, res) => {
+
+            const id = req.query.id;
+            console.log(req.query);
+            console.log("id:" + id)
+            const filter = { _id: ObjectId(id) };
+            console.log("filter: " + filter)
+            const updateDoc = {
+                $set: {
+                    pending: false
+                }
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc);
+            console.log(result);
+            res.json(result);
+        })
+
+
+
+        // post api  - add an order 
+        app.post('/order', async (req, res) => {
+
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            console.log(result);
+            res.json(result);
+        });
+
+        // get my orders api 
+        app.get('/orders/:email', async (req, res) => {
+
+            const mail = req.params.email;
+
+            const cursor = ordersCollection.find({ userMail: mail });
+            const orders = await cursor.toArray();
+            console.log("order: " + orders);
+            console.log('hiiting my orders');
+            res.send(orders);
+
+        });
+
+
+        // delete api 
+        app.delete('/bookings', async (req, res) => {
+
+            const id = req.query.id;
+            const query = { _id: ObjectId(id) };
+            console.log("id: " + id);
+            console.log("query: " + query);
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        });
+
+        // admin functions =====================================
+
+        // get all user orders 
+        app.get('/allorders', async (req, res) => {
+
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+
+        });
+
+    }
+    finally {
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Baiccha lon 200 deikkha lon 200..');
+});
+
+app.listen(port, () => {
+    console.log("Running server on port " + port);
+});
